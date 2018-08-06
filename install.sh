97a04086fbd4c2744a7d6da1ab5175d690fffbc1
aharshac <aharshac@gmail.com>
2018-08-06 17:03:05 +0530

    CHanged chromium config and html title

--- install.sh
+++ install.sh
@@ -13,11 +13,12 @@ cat > "/etc/X11/xinit/xinitrc" <<EOF
 # Run server
 cd /home/pi/HandheldHexUploader
 sudo npm run prod &
+sleep 8s
 
 # Start chromium in kiosk mode
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium-browser/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium-broswer/Default/Preferences
-chromium-browser --kiosk --incognito --start-maximized --noerrdialogs --disable-translate --disable-infobars --no-sandbox http://localhost:3000/
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+chromium-browser --window-size=480,320 --window-position=0,0 --start-fullscreen --no-sandbox --kiosk --incognito  --noerrdialogs --disable-translate --disable-infobars http://localhost:3000/ &
 
 # invoke global X session script
 . /etc/X11/Xsession
